21464cf2c2d87c654dc4e8d2a71c70e298048a2c
liaohuaqing <lhq77@163.com>
2018-11-22 17:13:13 +0800

    add数据库

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask,redirect,url_for
+from flask import Flask,render_template,request,url_for,send_from_directory,redirect,make_response
 #from tree_mold  import tree_mold
 #from tree import tree
 from about import aaa
-from flask import render_template,request,make_response
 import pymysql
 import os
 import re
@@ -10,9 +9,26 @@ import json
 import random
 import urllib
 import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_ckeditor import CKEditor, CKEditorField, upload_fail, upload_success
+
 app = Flask(__name__)
+ckeditor = CKEditor(app)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['CKEDITOR_SERVE_LOCAL'] = True
+app.config['CKEDITOR_HEIGHT'] = 400
+app.config['CKEDITOR_FILE_UPLOADER'] = 'upload'
+# app.config['CKEDITOR_ENABLE_CSRF'] = True  # if you want to enable CSRF protect, uncomment this line
+app.config['UPLOADED_PATH'] = os.path.join(basedir, 'uploads1')
+
+app.secret_key = 'secret string'
+
 app.register_blueprint(aaa, url_prefix="/")
  
+
 # 打开数据库连接
 db = pymysql.connect("localhost","root","liao1977","liao" )
 # 使用cursor()方法获取操作游标 
@@ -59,46 +75,58 @@ def gen_rnd_filename():
     filename_prefix = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
     return '%s%s' % (filename_prefix, str(random.randrange(1000, 10000)))#app.register_blueprint(tree, url_prefix="/")
 #app.register_blueprint(tree_mold, url_prefix="/ash")
+
+def upcontent(title,body):
+	
+	#print(body)
+	sql_insert ="insert into liaotb(name,content,date1,img,author) values('%s','%s','%s','%s','%s')" % (title,body,body[1],body[2],body[3])
+ 
+	try:
+		cursor.execute(sql_insert)
+		#提交
+		db.commit()
+	except Exception as e:
+		#错误回滚
+		print("bad!!")
+		db.rollback() 
+	finally:
+		print("ok!!")
+		#db.close()
+
 @app.route("/add", methods=['POST', 'GET'])
 def add():
-	return render_template("add.html")
-@app.route('/ckupload/', methods=['POST', 'OPTIONS'])
-def ckupload():
-    """CKEditor file upload"""
-    error = ''
-    url = ''
-    callback = request.args.get("CKEditorFuncNum")
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        body = form.body.data
+        # You may need to store the data in database here
+        upcontent(title,body)
+        return render_template('post.html', title=title, body=body)
+        
+    return render_template('add.html', form=form)
+class PostForm(FlaskForm):
+    title = StringField('Title')
+    body = CKEditorField('Body', validators=[DataRequired()])
+    submit = SubmitField()
 
-    if request.method == 'POST' and 'upload' in request.files:
-        fileobj = request.files['upload']
-        fname, fext = os.path.splitext(fileobj.filename)
-        rnd_name = '%s%s' % (gen_rnd_filename(), fext)
 
-        filepath = os.path.join(app.static_folder, 'upload', rnd_name)
 
-        # 检查路径是否存在，不存在则创建
-        dirname = os.path.dirname(filepath)
-        if not os.path.exists(dirname):
-            try:
-                os.makedirs(dirname)
-            except:
-                error = 'ERROR_CREATE_DIR'
-        elif not os.access(dirname, os.W_OK):
-            error = 'ERROR_DIR_NOT_WRITEABLE'
+@app.route('/files/<filename>')
+def uploaded_files(filename):
+    path = app.config['UPLOADED_PATH']
+    return send_from_directory(path, filename)
 
-        if not error:
-            fileobj.save(filepath)
-            url = url_for('static', filename='%s/%s' % ('upload', rnd_name))
-    else:
-        error = 'post error'
 
-    res = """<script type="text/javascript">
-  window.parent.CKEDITOR.tools.callFunction(%s, '%s', '%s');
-</script>""" % (callback, url, error)
+@app.route('/upload', methods=['POST'])
+def upload():
+    f = request.files.get('upload')
+    extension = f.filename.split('.')[1].lower()
+    if extension not in ['jpg', 'gif', 'png', 'jpeg']:
+        return upload_fail(message='Image only!')
+    f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+    url = url_for('uploaded_files', filename=f.filename)
+    return upload_success(url=url)
 
-    response = make_response(res)
-    response.headers["Content-Type"] = "text/html"
-    return response
 
 if __name__ == '__main__':
     app.run(port=8001)
