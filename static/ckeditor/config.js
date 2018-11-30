d4e9d42a1b9335ce1fb4c58927bee003392b83b8
liaohuaqing <lhq77@163.com>
2018-11-30 15:27:49 +0800

    修改editor

--- static/ckeditor/config.js
+++ static/ckeditor/config.js
@@ -1,6 +1,6 @@
 /**
- * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
+ * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 CKEDITOR.editorConfig = function( config ) {
@@ -36,5 +36,3 @@ CKEDITOR.editorConfig = function( config ) {
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
 };
-    // 上传图片路径
-    config.filebrowserImageUploadUrl = "/upload/";
